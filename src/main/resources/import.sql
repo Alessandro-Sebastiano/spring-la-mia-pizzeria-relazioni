d9025b6452b388fb21fb40a03e3481733393f8a5
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-12 21:10:28 +0200

    ingredients

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,12 @@ INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, m
 INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, mozzarella, salame piccante.', 'Diavola', 7);
 INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza funghi, salsiccia, mozzarella', 'Boscaiola', 5);
 
-INSERT INTO pizzas_db.offer(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title1', 1);
-INSERT INTO pizzas_db.offer(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title2', 1);
-INSERT INTO pizzas_db.offer(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title3', 2);
+INSERT INTO pizzas_db.offers(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title1', 1);
+INSERT INTO pizzas_db.offers(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title2', 1);
+INSERT INTO pizzas_db.offers(end_date, start_date, title, pizza_id) VALUES('2023-01-01', '2023-02-01', 'title3', 2);
+
+INSERT INTO pizzas_db.ingredients(name) VALUES('Pomodoro');
+INSERT INTO pizzas_db.ingredients(name) VALUES('Mozzarella');
+INSERT INTO pizzas_db.ingredients(name) VALUES('Funghi');
+INSERT INTO pizzas_db.ingredients(name) VALUES('Salame piccante');
+INSERT INTO pizzas_db.ingredients(name) VALUES('Salsiccia');
